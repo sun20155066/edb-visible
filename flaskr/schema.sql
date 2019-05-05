9cf6193da9e95705f1bedd4076846e009ebf386f
sunchao <2735896752@qq.com>
2019-05-05 22:08:41 +0800

    edb可视化程序

---
+++ flaskr/schema.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS edb CHARACTER SET utf8 COLLATE utf8_general_ci;
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username varchar(10) UNIQUE NOT NULL,
+  password varchar(200) NOT NULL
+);
+CREATE TABLE post (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  author_id INT NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title varchar(30) unique NOT NULL,
+  body TEXT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
