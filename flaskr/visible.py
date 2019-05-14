4cdc88f437106297080a55b6dbf5ebf1d453cae4
sunchao <2735896752@qq.com>
2019-05-14 15:32:56 +0800

    多个指标查看柱状图实现

--- flaskr/visible.py
+++ flaskr/visible.py
@@ -13,39 +13,75 @@ def barChart(theme):
         names = request.form.getlist('tablename')
         db = get_db()
         cur = db.cursor()
-        datas = []
-        for name in names:
-            cur.execute('SELECT * FROM `{0}`'.format(name))
-            datas.append(cur.fetchall())
-        X = []
-        Y = []
-        for data in datas:
+        count = len(names)
+        Ycount = 0
+
+        if count == 0:
+            sqlparm = ''
+            
+        elif count == 1:
+            sqlparm = 'SELECT `{0}`.A as date,`{0}`.B as `{0}` FROM `{0}`'.format(names[0])
+            cur.execute(sqlparm)
+            datas = cur.fetchall()
+
+            X = []
+            Y1 = []
+            Ycount = 1
+            for data in datas:
+                X.append(data['date'])
+                Y1.append(data[names[0]])
+
+            bar = Bar(init_opts=opts.InitOpts(theme=theme))
+            bar.add_yaxis(names[0], Y1)
+            bar.add_xaxis(X)
+            bar.set_global_opts(toolbox_opts=opts.ToolboxOpts())
+
+            
+        elif count == 2:
+            sqlparm = ' SELECT `{0}`.A as date,`{0}`.B as `{0}`, `{1}`.B as `{1}` FROM `{0}`,`{1}` where `{0}`.A = `{1}`.A '.format(names[0],names[1])
+            cur.execute(sqlparm)
+            datas = cur.fetchall()
+
+            X = []
+            Y1 = []
+            Y2 = []
+            Ycount = 2
+            for data in datas:
+                X.append(data['date'])
+                Y1.append(data[names[0]])
+                Y2.append(data[names[1]])
+
+            bar = Bar(init_opts=opts.InitOpts(theme=theme))
+            bar.add_yaxis(names[0], Y1)
+            bar.add_yaxis(names[1], Y2)
+            bar.add_xaxis(X)
+            bar.set_global_opts(toolbox_opts=opts.ToolboxOpts())
+
+        elif count == 3:
+            sqlparm = ' SELECT `{0}`.A as date,`{0}`.B as `{0}`, `{1}`.B as `{1}`, `{2}`.B as `{2}` FROM `{0}`,`{1}`,`{2}` where `{0}`.A = `{1}`.A and `{1}`.A = `{2}`.A '.format(names[0],names[1],names[2])
+            cur.execute(sqlparm)
+            datas = cur.fetchall()
+
+            X = []
+            Y1 = []
+            Y2 = []
+            Y3 = []
+            Ycount = 3
+            for data in datas:
+                X.append(data['date'])
+                Y1.append(data[names[0]])
+                Y2.append(data[names[1]])
+                Y3.append(data[names[2]])
+
+            bar = Bar(init_opts=opts.InitOpts(theme=theme))
+            bar.add_yaxis(names[0], Y1)
+            bar.add_yaxis(names[1], Y2)
+            bar.add_yaxis(names[2], Y3)
+            bar.add_xaxis(X)
+            bar.set_global_opts(toolbox_opts=opts.ToolboxOpts())
            
-            for j in data :
-                X.append(j['A'])
-                Y.append(j['B'])
 
-        # bar.add_xaxis(X)
-        # bar.add_yaxis("商家A", Y)
-        # bar.set_global_opts(title_opts=opts.TitleOpts(title="主标题", subtitle="副标题"))
-        # bar = Bar()
-        bar = (
-            Bar(init_opts=opts.InitOpts(theme=theme))
-            .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
-            .add_yaxis("商家A", [5, 20, 36, 10, 75, 90]) 
-            .add_yaxis("商家B", [25, 36, 10, 75, 90]) 
-            .set_global_opts(toolbox_opts=opts.ToolboxOpts(),datazoom_opts=[opts.DataZoomOpts()])
-        )
-
-    bar = (
-            Bar(init_opts=opts.InitOpts(theme=theme))
-            .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
-            .add_yaxis("商家A", [5, 20, 36, 10, 75, 90]) 
-            .add_yaxis("商家B", [25, 36, 10, 75, 90]) 
-            .set_global_opts(toolbox_opts=opts.ToolboxOpts())
-        )
-
-    return render_template('pyecharts.html',myechart=bar.render_embed())
+        return render_template('pyecharts.html',myechart=bar.render_embed())
 
 
 
