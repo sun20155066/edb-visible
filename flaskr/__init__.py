4e96154128679022769844240c1fb8f03d103e28
sunchao <2735896752@qq.com>
2019-05-17 12:55:41 +0800

    登录注册

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,4 @@
-from flask import (Flask,render_template)
+from flask import (Flask,render_template,redirect,url_for)
 
 
 def create_app():
@@ -22,7 +22,7 @@ def create_app():
 
     @app.route('/')
     def index():
-        return render_template('base.html')
+        return redirect(url_for('auth.login'))
 
     return app
 
