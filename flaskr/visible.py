1b62253888dc1cb57e25979d1df67fbf07b4ce26
sunchao <2735896752@qq.com>
2019-06-16 16:57:36 +0800

    实现n个表连接

--- flaskr/visible.py
+++ flaskr/visible.py
@@ -3,124 +3,58 @@ from pyecharts import options as opts
 from pyecharts.charts import Bar,Line,Scatter,Pie
 from flaskr.db import get_db
 from random import randrange
-
-
 from pyecharts.globals import ThemeType
 
 
 bp = Blueprint('visible', __name__)
 
 X = []
-Y1 = []
-Y2 = []
-Y3 = []
+Y = [[] for i in range(10)]
 names  = []
 theme = 'light'
-
 
 @bp.route('/saveData/<chartType>',methods=('GET','POST'))
 def saveData(chartType):
     if request.method == 'POST':
-        global names,X,Y1,Y2,Y3
+        global names
         names = request.form.getlist('tablename')
         db = get_db()
         cur = db.cursor()
         count = len(names)
-        Ycount = 0
 
         if count == 0:
             sqlparm = ''
             
-        elif count == 1:
-            sqlparm = 'SELECT `{0}`.A as date,`{0}`.B as `{0}` FROM `{0}`'.format(names[0])
+        elif count >0:
+            sqlparm = ' SELECT * FROM `{0}` '.format(names[0]) 
+            for i in range(1,count):
+                sqlparm += 'join `{0}` using(date) '.format(names[i])
             cur.execute(sqlparm)
             datas = cur.fetchall()
-
-            X = []
-            Y1 = []
-            Ycount = 1
-            for data in datas:
-                X.append(data['date'])
-                Y1.append(data[names[0]])
-
             
-        elif count == 2:
-            sqlparm = ' SELECT `{0}`.A as date,`{0}`.B as `{0}`, `{1}`.B as `{1}` FROM `{0}`,`{1}` where `{0}`.A = `{1}`.A '.format(names[0],names[1])
-            cur.execute(sqlparm)
-            datas = cur.fetchall()
-
-            X = []
-            Y1 = []
-            Y2 = []
-            Ycount = 2
             for data in datas:
                 X.append(data['date'])
-                Y1.append(data[names[0]])
-                Y2.append(data[names[1]])
-
-        elif count == 3:
-            sqlparm = ' SELECT `{0}`.A as date,`{0}`.B as `{0}`, `{1}`.B as `{1}`, `{2}`.B as `{2}` FROM `{0}`,`{1}`,`{2}` where `{0}`.A = `{1}`.A and `{1}`.A = `{2}`.A '.format(names[0],names[1],names[2])
-            cur.execute(sqlparm)
-            datas = cur.fetchall()
-
-            X = []
-            Y1 = []
-            Y2 = []
-            Y3 = []
-            Ycount = 3
-            for data in datas:
-                X.append(data['date'])
-                Y1.append(data[names[0]])
-                Y2.append(data[names[1]])
-                Y3.append(data[names[2]])
-           
+                for j in range(0,count):
+                   Y[j].append(data[names[j]])
+            
         return redirect(url_for('visible.visible',chartType='lineChart') )
 
 
 def bar_base() -> Bar:
     if len(names) == 0:
         pass
-    elif len(names) == 1:
-        c = (
-            Bar()
-            .add_xaxis(X)
-            .add_yaxis(names[0],Y1)
-            .set_global_opts(toolbox_opts=opts.ToolboxOpts(),
+    elif len(names) >= 1:
+        c = Bar()
+        c.add_xaxis(X)
+        c.set_global_opts(toolbox_opts=opts.ToolboxOpts(),
                             title_opts=opts.TitleOpts(title="BarChart"),
                             yaxis_opts=opts.AxisOpts(name="Data"),
                             xaxis_opts=opts.AxisOpts(name="Date"),
                             datazoom_opts=[opts.DataZoomOpts(), opts.DataZoomOpts(type_="inside")],
                             )
-        )
-
-    elif len(names) == 2:
-        c = (
-            Bar()
-            .add_xaxis(X)
-            .add_yaxis(names[0],Y1)
-            .add_yaxis(names[1],Y2)
-            .set_global_opts(toolbox_opts=opts.ToolboxOpts(),
-                            title_opts=opts.TitleOpts(title="BarChart"),
-                            yaxis_opts=opts.AxisOpts(name="Data"),
-                            xaxis_opts=opts.AxisOpts(name="Date"),
-                            datazoom_opts=[opts.DataZoomOpts(), opts.DataZoomOpts(type_="inside")],
-                            )
-        )
-
-    elif len(names) == 3:
-        c = (
-            Bar()
-            .add_xaxis(X)
-            .add_yaxis(names[0],Y1)
-            .add_yaxis(names[1],Y2)
-            .add_yaxis(names[2],Y3)
-            .set_global_opts(toolbox_opts=opts.ToolboxOpts(),
-                            title_opts=opts.TitleOpts(title="BarChart"),
-                            yaxis_opts=opts.AxisOpts(name="Data"),
-                            xaxis_opts=opts.AxisOpts(name="Date"),
-                            datazoom_opts=[opts.DataZoomOpts(), opts.DataZoomOpts(type_="inside")],
-                            )
-        )
+        
+        for i in range(0,len(names)):
+            c.add_yaxis(names[i],Y[i])
         
     return c
     
@@ -129,96 +63,38 @@ def line_base() -> Line:
     if len(names) == 0:
         pass
 
-    elif len(names) == 1:
-        c = (
-            Line()
-            .add_xaxis(X)
-            .add_yaxis(names[0],Y1)
-            .set_global_opts(toolbox_opts=opts.ToolboxOpts(),
+    elif len(names) >= 1:
+        c = Line()
+        c.add_xaxis(X) 
+        c.set_global_opts(toolbox_opts=opts.ToolboxOpts(),
                             title_opts=opts.TitleOpts(title="LineChart"),
                             yaxis_opts=opts.AxisOpts(name="Data"),
                             xaxis_opts=opts.AxisOpts(name="Date"),
                             datazoom_opts=[opts.DataZoomOpts(), opts.DataZoomOpts(type_="inside")],
                             )
-        )
-
-    elif len(names) == 2:
-        c = (
-            Line()
-            .add_xaxis(X)
-            .add_yaxis(names[0],Y1)
-            .add_yaxis(names[1],Y2)
-            .set_global_opts(toolbox_opts=opts.ToolboxOpts(),
-                            title_opts=opts.TitleOpts(title="LineChart"),
-                            yaxis_opts=opts.AxisOpts(name="Data"),
-                            xaxis_opts=opts.AxisOpts(name="Date"),
-                            datazoom_opts=[opts.DataZoomOpts(), opts.DataZoomOpts(type_="inside")],
-                            )
-        )
-
-    elif len(names) == 3:
-        c = (
-            Line()
-            .add_xaxis(X)
-            .add_yaxis(names[0],Y1)
-            .add_yaxis(names[1],Y2)
-            .add_yaxis(names[2],Y3)
-            .set_global_opts(toolbox_opts=opts.ToolboxOpts(),
-                            title_opts=opts.TitleOpts(title="LineChart"),
-                            yaxis_opts=opts.AxisOpts(name="Data"),
-                            xaxis_opts=opts.AxisOpts(name="Date"),
-                            datazoom_opts=[opts.DataZoomOpts(), opts.DataZoomOpts(type_="inside")],
-                            )
-        )
         
+        for i in range(0,len(names)):
+            c.add_yaxis(names[i],Y[i])
+
     return c
 
 def scatter_base() -> Scatter:
     if len(names) == 0:
         pass
 
-    elif len(names) == 1:
-        c = (
-            Scatter()
-            .add_xaxis(X)
-            .add_yaxis(names[0],Y1)
-            .set_global_opts(toolbox_opts=opts.ToolboxOpts(),
+    elif len(names) >= 1:
+        c = Scatter()
+        c.add_xaxis(X)
+        c.set_global_opts(toolbox_opts=opts.ToolboxOpts(),
                             title_opts=opts.TitleOpts(title="ScatterChart"),
                             yaxis_opts=opts.AxisOpts(name="Data"),
                             xaxis_opts=opts.AxisOpts(name="Date"),
                             datazoom_opts=[opts.DataZoomOpts(), opts.DataZoomOpts(type_="inside")],
                             )
-        )
-
-    elif len(names) == 2:
-        c = (
-            Scatter()
-            .add_xaxis(X)
-            .add_yaxis(names[0],Y1)
-            .add_yaxis(names[1],Y2)
-            .set_global_opts(toolbox_opts=opts.ToolboxOpts(),
-                            title_opts=opts.TitleOpts(title="ScatterChart"),
-                            yaxis_opts=opts.AxisOpts(name="Data"),
-                            xaxis_opts=opts.AxisOpts(name="Date"),
-                            datazoom_opts=[opts.DataZoomOpts(), opts.DataZoomOpts(type_="inside")],
-                            )
-        )
-
-    elif len(names) == 3:
-        c = (
-            Scatter()
-            .add_xaxis(X)
-            .add_yaxis(names[0],Y1)
-            .add_yaxis(names[1],Y2)
-            .add_yaxis(names[2],Y3)
-            .set_global_opts(toolbox_opts=opts.ToolboxOpts(),
-                            title_opts=opts.TitleOpts(title="ScatterChart"),
-                            yaxis_opts=opts.AxisOpts(name="Data"),
-                            xaxis_opts=opts.AxisOpts(name="Date"),
-                            datazoom_opts=[opts.DataZoomOpts(), opts.DataZoomOpts(type_="inside")],
-                            )
-        )
         
+        for i in range(0,len(names)):
+            c.add_yaxis(names[i],Y[i])
+
     return c
 
 @bp.route("/barChart",methods=('GET','POST'))
@@ -247,6 +123,4 @@ def setTheme(setTheme):
     global theme
     theme = setTheme
     before_url = request.referrer
-
-    print('before_url======',before_url)
     return redirect(before_url)
